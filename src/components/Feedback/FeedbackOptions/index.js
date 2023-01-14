190c9ce7e0ef012e75374df5fc57a0ff272f3ceb
namhels <8.lsv.888@gmail.com>
2023-01-14 18:30:34 +0300

    delete supporting files

--- src/components/Feedback/FeedbackOptions/index.js
+++
@@ -1,2 +0,0 @@
-export { default } from './FeedbackOptions';
-export * from './FeedbackOptions.Styled';
--- src/components/Feedback/Statistics/index.js
+++
@@ -1,2 +0,0 @@
-export { default } from './Statistics';
-export * from './Statistics.Styled';
